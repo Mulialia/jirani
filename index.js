06129415c1c12e8e8a38badceec8904213c6305e
Mulialia <mulialia@gmail.com>
2018-12-21 14:15:41 +0300

    Adjusted port reading

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var PORT = process.env.PORT || 3000
+
 app.get('/',function(req, res){
     res.sendFile(__dirname+'/index.html');
 })
@@ -26,6 +28,6 @@ io.on('connection',function(socket){
         console.log('one user disconnected '+socket.id);
     })
 })
-http.listen(3000,function(){
-    console.log('Server listening on port 3000');
+http.listen(PORT,function(){
+    console.log('Server listening on port '+PORT);
 })
