35ca0e8338fd9fb9c06d02ee42d0bcd2619d4e51
Mulialia <mulialia@gmail.com>
2018-12-30 18:06:24 +0300

    Fourth Clean Commit

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+  socket.on('message', function(data){
+    data.text = 'Welcome to the world!';
+    data.sender = 'Being Grateful';
+    socket.emit('message',{message:data});
+    console.log(data);
+      });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
