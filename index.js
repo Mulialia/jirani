50cf60212e6d67b16fc083996b90ee3c8a9e2dfa
Mulialia <mulialia@gmail.com>
2018-12-30 11:47:30 +0300

    First recommit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000
 
 app.get('/',function(req, res){
