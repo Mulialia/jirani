8b73520627914b98c5a19481b941ad26caa050d9
Mulialia <mulialia@gmail.com>
2018-12-30 13:15:07 +0300

    Third try

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io').listen(http);
-const PORT = process.env.PORT || 3000
+// const io = require('socket.io').listen(http);
+const PORT = process.env.PORT || 3000;
 
 app.get('/',function(req, res){
     res.sendFile(__dirname+'/index.html');
